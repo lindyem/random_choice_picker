ab51551d6801dc6f8cdde2ffb62f65fdff5d33ce
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-02-25 11:55:29 -0800

    fixed typo

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  
 })
 
 function createTags(input) {
-  const tags = input.split(',')
-  
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+  tagsEl.innerHTML = ''
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
+  })
 }
